ca79c844c06e45719a4374de8b7ce44e3a8ea042
Kacper Pikulski <pikulak1@gmail.com>
2016-11-12 00:29:11 +0100

    deleted:    api/blueprints.py 	deleted:    api/context_processors.py

--- api/context_processors.py
+++
@@ -1,8 +0,0 @@
-from pywdbms.db.containers import BindContainer
-from pywdbms.api.blueprints import Blueprints
-
-blueprint = Blueprints.get("blueprint")
-@blueprint.context_processor
-def get_table_names(shortname):
-	_, meta = BindContainer.get(shortname)
-	return meta.tables.keys()
