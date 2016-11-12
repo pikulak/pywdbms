9b253b695af4af1b577a74cbf41be85ca1f4f7b6
Kacper Pikulski <pikulak1@gmail.com>
2016-11-12 04:23:35 +0100

    modified:   api/app.py 	modified:   templates/base.html 	modified:   templates/table.html

--- api/app.py
+++ api/app.py
@@ -12,7 +12,7 @@ from sqlalchemy import select
 blueprint = Blueprint('blueprint', __name__, template_folder="../templates")
 load()
 BindContainer.add("short_db_name")
-
+default_offset = 25
 @blueprint.route('/')
 def dashboard():
     resp = make_response(render_template('dashboard.html'), 200)
@@ -26,18 +26,48 @@ def server_view(host):
                         databases=databases, 
                         binds=BindContainer.BINDS), 200)
 
-@blueprint.route('/servers/<host>/databases/<shortname>/tables/<table_name>')
-def table_view(host, shortname, table_name):
+
+@blueprint.route('/servers/<host>/databases/<shortname>/tables/<table_name>/<offset>/<page>')
+@blueprint.route('/servers/<host>/databases/<shortname>/tables/<table_name>/')
+def table_view(host, shortname, table_name, offset=None, page=None):
+    if offset is None:
+        offset = default_offset
+    if page is None:
+        page = 1
+
+    table_url = "/servers/{}/databases/{}/tables/{}".format(host, shortname, table_name)
+    if int(page) <= 0:
+        page = 1
+    if int(page) -1 <= 0:
+        prev_ = False
+    else:
+        prev_ = int(page) - 1
+    offset_ = (int(offset) * int(page)) - int(offset)
     databases = DatabaseContainer.get_databases(host=host)
     connection, meta = BindContainer.get(shortname)
     table = meta.tables[table_name]
-    result = connection.execute(table.select())
+    result = connection.execute(table.select().limit(offset).offset(offset_))
+    rows = result.fetchall()
+    types = [col.type for col in result.context.compiled.statement.columns]
+    if len(rows) < int(offset):
+        next_ = False
+    else:
+        next_ = int(page) + 1
     return make_response(render_template(
                         'table.html',
                         databases=databases,
                         table_name=table_name,
                         keys=result.keys(),
-                        rows=result.fetchall()), 200)
+                        rows=rows,
+                        types=types,
+                        current_page=page,
+                        table_url=table_url,
+                        offset=offset,
+                        prev=prev_,
+                        next=next_), 200)
+ 
+
+"""CONTEXT PROCESSORS"""   
 
 @blueprint.context_processor
 def utility_processor():
