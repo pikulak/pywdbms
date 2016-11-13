c1563cfedf6d4514964e5616ac6483599c9e8a2c
Kacper Pikulski <pikulak1@gmail.com>
2016-11-13 04:00:18 +0100

    modified:   db/containers.py 	modified:   db/statements.py 	modified:   templates/table.html

--- db/containers.py
+++ db/containers.py
@@ -6,80 +6,80 @@ from pywdbms.utils.custom_dict_filter import custom_dict_filter as c_d_f
 from pywdbms.db.statements import StatementsChooser
 
 class DatabaseContainer(object):
-	DATABASES = {}
-	UNIQUE_HOSTS = []
+    DATABASES = {}
+    UNIQUE_HOSTS = []
 
-	@staticmethod
-	def get_databases(**kwargs):
-		_DATABASES = getattr(DatabaseContainer, "DATABASES")
-		if len(kwargs) <= 0:
-			return _DATABASES
-		else:
-			return [{shortname: properties}
-					for shortname, properties
-					 in _DATABASES.items()
-					  if c_d_f(properties, kwargs) is True]
+    @staticmethod
+    def get_databases(**kwargs):
+        _DATABASES = getattr(DatabaseContainer, "DATABASES")
+        if len(kwargs) <= 0:
+            return _DATABASES
+        else:
+            return [{shortname: properties}
+                    for shortname, properties
+                     in _DATABASES.items()
+                      if c_d_f(properties, kwargs) is True]
 
-	@staticmethod
-	def get_uniquehosts():
-		return getattr(DatabaseContainer, "UNIQUE_HOSTS")
+    @staticmethod
+    def get_uniquehosts():
+        return getattr(DatabaseContainer, "UNIQUE_HOSTS")
 
-	@staticmethod
-	def get(shortname):
-		return getattr(DatabaseContainer, "DATABASES")[shortname]
+    @staticmethod
+    def get(shortname):
+        return getattr(DatabaseContainer, "DATABASES")[shortname]
 
-	@staticmethod
-	def add(shortname, properties):
-		UNIQUE_HOSTS = getattr(DatabaseContainer, "UNIQUE_HOSTS")
-		if properties["host"] not in UNIQUE_HOSTS:
-			UNIQUE_HOSTS.append(properties["host"])
-		_databases = getattr(DatabaseContainer, "DATABASES")
-		_databases[shortname] = properties
-		setattr(DatabaseContainer, "DATABASES", _databases)
+    @staticmethod
+    def add(shortname, properties):
+        UNIQUE_HOSTS = getattr(DatabaseContainer, "UNIQUE_HOSTS")
+        if properties["host"] not in UNIQUE_HOSTS:
+            UNIQUE_HOSTS.append(properties["host"])
+        _databases = getattr(DatabaseContainer, "DATABASES")
+        _databases[shortname] = properties
+        setattr(DatabaseContainer, "DATABASES", _databases)
 
-	@staticmethod
-	def load_databases(databases={}):
-		for shortname, properties in databases.items():
-			DatabaseContainer.add(shortname, properties)
+    @staticmethod
+    def load_databases(databases={}):
+        for shortname, properties in databases.items():
+            DatabaseContainer.add(shortname, properties)
 
 
 
 class BindContainer(object):
-	BINDS = {}
+    BINDS = {}
 
-	@staticmethod
-	def get_all():
-		return getattr(BindContainer, "BINDS")
+    @staticmethod
+    def get_all():
+        return getattr(BindContainer, "BINDS")
 
-	@staticmethod
-	def get(shortname):
-		return getattr(BindContainer, "BINDS")[shortname]
+    @staticmethod
+    def get(shortname):
+        return getattr(BindContainer, "BINDS")[shortname]
 
-	@staticmethod
-	def add(shortname):
-		_db_properties = DatabaseContainer.get(shortname)
-		_url = url.URL(drivername=_db_properties["drivername"],
-				   username=_db_properties["username"],
-				   password=_db_properties["password"],
-				   host=_db_properties["host"],
-				   port=_db_properties["port"],
-				   database=_db_properties["database"])
-		_engine = create_engine(_url, encoding="utf8")
-		_connection = _engine.connect()
-		_meta = MetaData()
-		_meta.reflect(bind=_engine, autoload=True)
-		_BINDS = getattr(BindContainer, "BINDS")
-		_BINDS[shortname] = (_connection,
-							 _meta, 
-							 StatementsChooser.for_[_db_properties["drivername"]])
-		setattr(BindContainer, "BINDS", _BINDS)
+    @staticmethod
+    def add(shortname):
+        _db_properties = DatabaseContainer.get(shortname)
+        _url = url.URL(drivername=_db_properties["drivername"],
+                   username=_db_properties["username"],
+                   password=_db_properties["password"],
+                   host=_db_properties["host"],
+                   port=_db_properties["port"],
+                   database=_db_properties["database"])
+        _engine = create_engine(_url, encoding="utf8")
+        _connection = _engine.connect()
+        _meta = MetaData()
+        _meta.reflect(bind=_engine, autoload=True)
+        _BINDS = getattr(BindContainer, "BINDS")
+        _BINDS[shortname] = (_connection,
+                             _meta, 
+                             StatementsChooser.for_[_db_properties["drivername"]])
+        setattr(BindContainer, "BINDS", _BINDS)
 
-	@staticmethod
-	def delete(shortname):
-		_BINDS = getattr(BindContainer, "BINDS")
-		_BINDS[shortname].close()
-		del _BINDS[shortname]
-		setattr(BindContainer, "BINDS", _BINDS)
+    @staticmethod
+    def delete(shortname):
+        _BINDS = getattr(BindContainer, "BINDS")
+        _BINDS[shortname].close()
+        del _BINDS[shortname]
+        setattr(BindContainer, "BINDS", _BINDS)
 
 
 
--- db/statements.py
+++ db/statements.py
@@ -1,25 +1,25 @@
 import abc
 
 class StatementsBaseAbstract(metaclass=abc.ABCMeta):
-	@staticmethod
-	@abc.abstractmethod
-	def get_database_encoding(database):
-		raise NotImplementedError('You must define get_server_encoding method')
+    @staticmethod
+    @abc.abstractmethod
+    def get_database_encoding(database):
+        raise NotImplementedError('You must define get_server_encoding method')
 
-	@staticmethod
-	@abc.abstractmethod
-	def get_server_version():
-		raise NotImplementedError('You must define get_server_encoding method')
+    @staticmethod
+    @abc.abstractmethod
+    def get_server_version():
+        raise NotImplementedError('You must define get_server_encoding method')
 
 class PostgresqlStatements(StatementsBaseAbstract):
 
-	def get_database_encoding(database):
-		stmt = "SELECT pg_encoding_to_char(encoding) FROM pg_database WHERE datname='{}'".format(database)
-		return stmt
+    def get_database_encoding(database):
+        stmt = "SELECT pg_encoding_to_char(encoding) FROM pg_database WHERE datname='{}'".format(database)
+        return stmt
 
-	def get_server_version():
-		stmt = "SELECT version()"
-		return stmt
+    def get_server_version():
+        stmt = "SELECT version()"
+        return stmt
 
 class StatementsChooser(object):
-	for_ = {"postgresql+psycopg2": PostgresqlStatements}
+    for_ = {"postgresql+psycopg2": PostgresqlStatements}
