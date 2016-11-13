3eeb8ce2d6d13020dce289850c0fe5bc11803db3
Kacper Pikulski <pikulak1@gmail.com>
2016-11-13 04:02:53 +0100

    modified:   db/file.py 	modified:   utils/custom_dict_filter.py

--- utils/custom_dict_filter.py
+++ utils/custom_dict_filter.py
@@ -1,12 +1,12 @@
 def custom_dict_filter(d, conditions):
-	for key, value in conditions.items():
-		if d[key] != value:
-			return False
-	return True
+    for key, value in conditions.items():
+        if d[key] != value:
+            return False
+    return True
 
 ####tests####
 #first = {"host": "192.168.1.104",
-#		 "port": 5432}
+#        "port": 5432}
 #second = {"host": "192.168.1.104"}
 #
 #print(custom_dict_filter(first, second))
