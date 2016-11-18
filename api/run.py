30627ea8b143c88d2cb291c3649cea1859bb5cc7
Kacper Pikulski <pikulak1@gmail.com>
2016-11-19 00:37:56 +0100

    Fixed path bugs

--- api/run.py
+++ api/run.py
@@ -1,11 +1,16 @@
 #!c:/python34/python.exe
 import sys
 import os
-dir_ = os.path.dirname(os.path.join(os.path.realpath(__file__), "../../../"))
-sys.path.insert(0, dir_)
+try:
+	from pathlib import Path
+	p = str(Path(__file__).parents[2])
+except ImportError:
+	import os
+	p = os.path.dirname(os.path.join(os.path.realpath(__file__), "../../../"))
+	
+sys.path.insert(0, p)
 from flask import Flask
 from pywdbms.api.app import blueprint
-
 app = Flask(__name__)
 app.secret_key = 'some_secret'
 app.register_blueprint(blueprint)
